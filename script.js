a84608296a5d187c88f89f3359da8ad263196b59
unknown <you@example.com>
2026-07-19 17:05:49 +0900

    文字が変わるコードを確実に保存

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function sayHello() {
-    alert("こんにちは！JavaScriptが正しく動いています！");
+function changeText() {
+    // 1. htmlの中から idが "title" の要素（h1）を探して、変数 target に入れます
+    let target = document.getElementById("title");
+
+    // 2. その要素の中身のテキスト（innerText）を書き換えます
+    target.innerText = "ボタンが押されたので、文字が書き換わりました！";
 }
